ca73bde80c3980128dd8e17db24cd110688e04d2
Norbot <nhoenderop@gmail.com>
2018-05-22 14:41:07 +0200

    Added a camera library and set it up, made sure that subject number can be provided and is required to start any activity. It stores this value in the app preferences. Retrieve it by using:
    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
    prefs.getInt("subjectnr", -1);

--- app/src/main/java/nl/norbot/senseswipe/CameraActivity.java
+++ app/src/main/java/nl/norbot/senseswipe/CameraActivity.java
@@ -3,11 +3,44 @@ package nl.norbot.senseswipe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wonderkiln.camerakit.CameraKit;
+import com.wonderkiln.camerakit.CameraView;
+
 public class CameraActivity extends AppCompatActivity {
+
+    CameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        cameraView = findViewById(R.id.camera);
+        cameraView.setFacing(CameraKit.Constants.FACING_BACK);
+
+        /*
+        // Taking a picture
+        cameraView.captureImage(new CameraKitView.ImageCallback() {
+            @Override
+            public void onImage(CameraKitView view, byte[] jpeg) {
+
+            }
+        });
+        */
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
+    }
+
+
+
 }
--- app/src/main/java/nl/norbot/senseswipe/MainActivity.java
+++ app/src/main/java/nl/norbot/senseswipe/MainActivity.java
@@ -1,18 +1,35 @@
 package nl.norbot.senseswipe;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.ContentValues;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Integer subjectNumber;
+    Button maze, camera, typing, saveButton;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        /*
 
         DatabaseHelper mDbHelper = new DatabaseHelper(getApplicationContext());
 
@@ -26,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
 
         // Insert the new row, returning the primary key value of the new row
         long newRowId = db.insert(DBContract.DBEntry.TABLE_NAME, null, values);
+
+        */
+
+        editText = findViewById(R.id.edittext_subject_number);
+        saveButton = findViewById(R.id.button_save_subject_number);
+        maze = findViewById(R.id.button_maze);
+        camera = findViewById(R.id.button_camera);
+        typing = findViewById(R.id.button_typing);
+
+        subjectNumber = prefs.getInt("subjectnr", -1);
+        if(subjectNumber > 0){
+            maze.setEnabled(true);
+            camera.setEnabled(true);
+            typing.setEnabled(true);
+            editText.setText("" + subjectNumber);
+        }
 
     }
 
@@ -43,4 +76,17 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, TypingActivity.class);
         startActivity(intent);
     }
+
+    public void saveSubjectNumber(View view) {
+        subjectNumber = Integer.parseInt(editText.getText().toString());
+        prefs.edit().putInt("subjectnr", subjectNumber).commit();
+        editText.setInputType(InputType.TYPE_NULL);
+        editText.setText("Subject number set to " + subjectNumber);
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        maze.setEnabled(true);
+        camera.setEnabled(true);
+        typing.setEnabled(true);
+    }
+
 }
