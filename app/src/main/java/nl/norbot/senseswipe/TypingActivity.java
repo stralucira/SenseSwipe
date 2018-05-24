8008ed97cbd8536c71a6242183c6ab1ca6b47bfc
Başar Oğuz <baoguz@gmail.com>
2018-05-24 16:23:38 +0200

    Typo correction speed is now calculated and logged upon correction

--- app/src/main/java/nl/norbot/senseswipe/TypingActivity.java
+++ app/src/main/java/nl/norbot/senseswipe/TypingActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.widget.EditText;
 import android.view.inputmethod.*;
 import android.widget.Button;
 import android.view.View;
+import android.text.TextWatcher;
 
 
 public class TypingActivity extends AppCompatActivity {
@@ -18,6 +20,7 @@ public class TypingActivity extends AppCompatActivity {
     EditText editText;
     int textLength;
     int cursorOffsetFromEnd = 0;
+    long start, end;
 
     private static final String TAG = TypingActivity.class.getSimpleName();
 
@@ -60,6 +63,31 @@ public class TypingActivity extends AppCompatActivity {
         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.d(TAG, "Text changed to: " + editText.getText().toString());
+                if (editText.getText().toString().equals("mistyped")){
+                    Log.d(TAG, "Success ");
+                    end = System.currentTimeMillis();
+                    long time = end - start;
+                    Log.d(TAG, "Typo fixed in " + time + " msecs.");
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+
+            }
+        });
+
         setUpButton();
     }
 
@@ -70,6 +98,7 @@ public class TypingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.i("Start Button", "start button clicked");
+                start = System.currentTimeMillis();
                 //finish();
             }
         });
