356e0ffb4a711854a5aa927387dee991c4072c62
Dimas Tichelaar <dimas.tichelaar@gmail.com>
2018-05-29 15:17:33 +0200

    Added DDR Activity and button to Main Activity

---
+++ app/src/main/java/nl/norbot/senseswipe/DDRActivity.java
@@ -0,0 +1,13 @@
+package nl.norbot.senseswipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DDRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ddr);
+    }
+}
