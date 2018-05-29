2f72cb4d09455f350ee9eb42d665399c73e53596
Dimas Tichelaar <dimas.tichelaar@gmail.com>
2018-05-29 22:11:42 +0200

    Added swipe handler, arrows set to invisible by default

--- app/src/main/java/nl/norbot/senseswipe/DDRActivity.java
+++ app/src/main/java/nl/norbot/senseswipe/DDRActivity.java
@@ -1,13 +1,43 @@
 package nl.norbot.senseswipe;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DDRActivity extends AppCompatActivity {
+    private final String TAG = getClass().getSimpleName();
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                Log.d(TAG, Integer.toString(intent.getIntExtra("gesture_id", 0)));
+
+                // TODO: Implement input handling here.
+                // For direction codes see https://developer.android.com/reference/android/accessibilityservice/FingerprintGestureController#FINGERPRINT_GESTURE_SWIPE_DOWN
+            } catch (Exception e){
+                Log.d(TAG, e.toString());
+            }
+        }
+    };
+
+    private ImageView arrowUp, arrowDown, arrowLeft, arrowRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ddr);
+        arrowUp = findViewById(R.id.arrow_up);
+        arrowDown = findViewById(R.id.arrow_down);
+        arrowLeft = findViewById(R.id.arrow_left);
+        arrowRight = findViewById(R.id.arrow_right);
+        arrowUp.setVisibility(View.INVISIBLE);
+        arrowDown.setVisibility(View.INVISIBLE);
+        arrowLeft.setVisibility(View.INVISIBLE);
+        arrowRight.setVisibility(View.INVISIBLE);
     }
 }
