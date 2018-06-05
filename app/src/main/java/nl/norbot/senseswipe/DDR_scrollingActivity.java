1e2e3b932c0d329a8fe288767695985b2cd33847
Milan Wijnmaalen <m.wijnmaalen@students.uu.nl>
2018-06-05 13:37:38 +0200

    asfdadf

--- app/src/main/java/nl/norbot/senseswipe/DDR_scrollingActivity.java
+++ app/src/main/java/nl/norbot/senseswipe/DDR_scrollingActivity.java
@@ -1,13 +1,61 @@
 package nl.norbot.senseswipe;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DDR_scrollingActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
+    private int id;
+    private boolean usefingerprintgestures;
+    private boolean usescreengestures;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                Log.d(TAG, Integer.toString(intent.getIntExtra("gesture_id", 0)));
+
+                // TODO: Implement input handling here.
+                // For direction codes see https://developer.android.com/reference/android/accessibilityservice/FingerprintGestureController#FINGERPRINT_GESTURE_SWIPE_DOWN
+            } catch (Exception e){
+                Log.d(TAG, e.toString());
+            }
+        }
+    };
+
+    Integer subjectNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ddr_scrolling);
+
+        id = getIntent().getIntExtra("id", 0);
+        usefingerprintgestures = getIntent().getBooleanExtra("useFingerprint", false);
+        usescreengestures = !usefingerprintgestures;
     }
+
+    protected void onResume() {
+        super.onResume();
+
+        registerReceiver(receiver, new IntentFilter("FINGERPRINT_GESTURE_DETECTED"));
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "Unregistering receiver.");
+        unregisterReceiver(receiver);
+    }
+
 }
