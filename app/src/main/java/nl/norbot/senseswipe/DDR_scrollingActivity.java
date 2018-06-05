58d78ed05def9d8ad352eeba7b6cad6d98469f38
Milan Wijnmaalen <m.wijnmaalen@students.uu.nl>
2018-06-05 13:16:20 +0200

    Created activity

---
+++ app/src/main/java/nl/norbot/senseswipe/DDR_scrollingActivity.java
@@ -0,0 +1,13 @@
+package nl.norbot.senseswipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DDR_scrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ddr_scrolling);
+    }
+}
